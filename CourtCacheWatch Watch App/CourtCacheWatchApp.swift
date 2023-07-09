2805053fe73b9a781b2299ccbbbc1468569864fc
Yu Xuan Yio <yioyuxuan@gmail.com>
2023-07-10 01:48:05 +1000

    Added .gitignore

---
+++ CourtCacheWatch Watch App/CourtCacheWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourtCacheWatchApp.swift
+//  CourtCacheWatch Watch App
+//
+//  Created by Yu Xuan Yio on 19/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct CourtCacheWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
